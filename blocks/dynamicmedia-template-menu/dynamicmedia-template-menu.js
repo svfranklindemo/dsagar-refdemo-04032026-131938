668dd3a39cb583525f0586892940e86b8df75aae
Dipen Sagar <dipenrique@gmail.com>
2026-03-04 22:01:39 +0530

    Adding Dynamic Media Template Menu block

--- blocks/dynamicmedia-template-menu/dynamicmedia-template-menu.js
+++ blocks/dynamicmedia-template-menu/dynamicmedia-template-menu.js
@@ -41,7 +41,10 @@ function buildTemplateUrl(state) {
   params.set('$price', state.price.replace(/\s/g, ''));
   const headlineEncoded = state.headline.replace(/\n/g, ' \\par ').trim();
   params.set('$headline', headlineEncoded);
-  const query = params.toString().replace("%24", '$').replace("%2F", '/').replace("%2520", '%20');
+  var query = params.toString().replace("%24", '$')
+  query = query.replace("%2F", '/')
+  query = query.replace("%2520", '%20');
+  query = query.replace("%24", '$');
   // const query = params.toString().replace(/\+/g, '%20');
   return `${DEFAULT_TEMPLATE_BASE}?${query}`;
 }
